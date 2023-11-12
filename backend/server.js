1ef68faa17d5213156fab232303bb13b3483f1fc
Jeyms <james.boac@gmail.com>
2023-11-12 12:21:19 +0800

    Deployed!

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ app.put('/api/items/:name', async (req, res) => {
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) => 
-res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
 
 app.use((err, req, res, next) => {
